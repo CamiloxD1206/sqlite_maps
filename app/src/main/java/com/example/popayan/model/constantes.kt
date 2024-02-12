298c56fb37bb4d7a9141a6b453506e55fd9fcb28
Camilo Tacue Salazar <cetacue@misena.edu.co>
2024-02-12 12:00:14 -0500

    falta el sqlite

---
+++ app/src/main/java/com/example/popayan/model/constantes.kt
@@ -0,0 +1,12 @@
+package com.example.popayan.model
+
+class constantes {
+    companion object {
+        const val NOM_BD = "BdMaps"
+        const val VERSION_BD = 1
+
+        const val TABLA = "CREATE TABLE coodenadas(cod INTEGER, latitud REAL, longitud REAL, title TEXT)"
+
+
+    }
+}
